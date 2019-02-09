efedaa694b6f3b4492e8a690581ca70869b5763f
Neo GMDXX <40813982+gmdxx@users.noreply.github.com>
2019-02-08 18:28:06 -0600

    > Carga de archivos locales

---
+++ monitor.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+datetime=$(date "+%Y-%m-%d %H:%M:%S")
+file="/var/www/html/index.json"
+
+echo "date: $datetime" > monitor.log
+echo "user: $USER" >> monitor.log
+/sbin/service mysqld status >> monitor.log
+
+/sbin/service mysqld status | grep "run" >> /dev/null
+
+
+code=$?
+
+if [ $code = "0" ]
+then
+        status="OK"
+else
+        status="ERROR"
+fi
+
+
+cat << EOF > $file
+{
+    "last_task_execution": "$datetime",
+    "mysqld_code": "$code",
+    "mysql_status": "$status",
+    "sys_path": "$PATH"
+}
+EOF
